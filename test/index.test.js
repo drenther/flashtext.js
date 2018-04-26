12d39069b8b3d1bfcc80d060d9ea877d7299fe20
Soumyajit Pathak <s.pathak5995@gmail.com>
2018-04-27 02:37:58 +0530

    100% function test coverage

--- test/index.test.js
+++ test/index.test.js
@@ -73,6 +73,27 @@ describe('basic functionalities', () => {
 		expect(keywordsFound).toEqual(['react']);
 	});
 
+	test('remove keywords from object test', () => {
+		const processor = new KeywordProcessor();
+
+		processor.addKeywordsFromObject({
+			java: ['java_2e', 'java programming'],
+			python: ['python2.7', 'python programming'],
+		});
+
+		let keywordsFound = processor.extractKeywords('I love java_2e and python programming.');
+		expect(keywordsFound).toEqual(['java', 'python']);
+
+		processor.removeKeywordsFromObject({ java: ['java_2e', 'java programming'] });
+
+		keywordsFound = processor.extractKeywords('I love java_2e and python programming.');
+		expect(keywordsFound).toEqual(['python']);
+
+		const sentence = 'I prefer python over java programming.';
+		const newSentence = processor.replaceKeywords(sentence);
+		expect(newSentence).toEqual(sentence);
+	});
+
 	test('add characters as part of word characters', () => {
 		const processor = new KeywordProcessor();
 
@@ -85,17 +106,43 @@ describe('basic functionalities', () => {
 		const after = processor.extractKeywords('I love Big Apple/Bay Area.');
 		expect(after).toEqual([]);
 	});
+
+	test('set non word boundaries', () => {
+		const processor = new KeywordProcessor();
+		const arrayOfKeywords = ['java-script', 'javascript', 'Javascript'];
+		processor.addKeywordsFromObject({
+			JavaScript: arrayOfKeywords,
+		});
+
+		const result = 'I love JavaScript';
+		arrayOfKeywords.forEach(keyword => {
+			const sentence = `I love ${keyword}`;
+			expect(processor.replaceKeywords(sentence)).toEqual(result);
+		});
+
+		processor.setNonWordBoundaries([' ']);
+		const sentence = 'I love java-script';
+		expect(processor.extractKeywords(sentence)).toEqual([]);
+		expect(processor.replaceKeywords(sentence)).toEqual(sentence);
+	});
 });
 
 describe('common use cases', () => {
 	test('shorter search string then keywords', () => {
-		const processor = new KeywordProcessor();
-
-		processor.addKeyword('Google', 'Alphabet');
+		const processorCaseInsensitive = new KeywordProcessor();
+		const processorCaseSensitive = new KeywordProcessor(true);
 
 		const before = 'I work at Google';
 		const after = 'I work at Alphabet';
-		expect(processor.replaceKeywords(before)).toEqual(after);
+
+		processorCaseInsensitive.addKeyword('Google', 'Alphabet');
+		expect(processorCaseInsensitive.replaceKeywords(before)).toEqual(after);
+
+		const sentence = 'I work google';
+
+		processorCaseSensitive.addKeyword('Google', 'Alphabet');
+		expect(processorCaseSensitive.replaceKeywords(before)).toEqual(after);
+		expect(processorCaseSensitive.replaceKeywords(sentence)).toEqual(sentence);
 	});
 });
 
