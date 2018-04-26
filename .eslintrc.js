05c2033fd4f6172016ac538c98700b03783882cc
Soumyajit Pathak <s.pathak5995@gmail.com>
2018-04-26 20:54:59 +0530

    restructure testing setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,18 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	env: {
+		es6: true,
+		node: true,
+		'jest/globals': true,
+	},
+	plugins: ['jest', 'prettier'],
+	extends: ['eslint:recommended', 'plugin:jest/recommended', 'prettier'],
+	parserOptions: {
+		sourceType: 'module',
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,115 +1,117 @@
 const KeywordProcessor = require('../build/index');
 
-test('extract keywords test', () => {
-	const processor = new KeywordProcessor();
+describe('basic functionalities', () => {
+	test('extract keywords test', () => {
+		const processor = new KeywordProcessor();
 
-	processor.addKeyword('Big Apple', 'New York');
-	processor.addKeyword('Bay Area');
+		processor.addKeyword('Big Apple', 'New York');
+		processor.addKeyword('Bay Area');
 
-	const keywordsFound = processor.extractKeywords(
-		'I love Big Apple and Bay Area.'
-	);
-	const expected = ['New York', 'Bay Area'];
+		const keywordsFound = processor.extractKeywords('I love Big Apple and Bay Area.');
+		const expected = ['New York', 'Bay Area'];
 
-	expect(keywordsFound).toEqual(expected);
-});
-
-test('replace keywords test', () => {
-	const processor = new KeywordProcessor();
-
-	processor.addKeyword('Big Apple', 'New York');
-	processor.addKeyword('New Delhi', 'NCR Region');
-
-	const newSentence = processor.replaceKeywords(
-		'I love Big Apple and new delhi.'
-	);
-	const expected = 'I love New York and NCR Region.';
-
-	expect(newSentence).toEqual(expected);
-});
-
-test('case sensitive example test', () => {
-	const processor = new KeywordProcessor(true);
-
-	processor.addKeyword('Big Apple', 'New York');
-	processor.addKeyword('Bay Area');
-
-	const keywordsFound = processor.extractKeywords(
-		'I love big Apple and Bay Area.'
-	);
-	const expected = ['Bay Area'];
-
-	expect(keywordsFound).toEqual(expected);
-});
-
-test('add multiple keywords using Object test', () => {
-	const processor = new KeywordProcessor();
-
-	processor.addKeywordsFromObject({
-		java: ['java_2e', 'java programming'],
-		python: ['python2.7', 'python programming'],
+		expect(keywordsFound).toEqual(expected);
 	});
 
-	const keywordsFound = processor.extractKeywords(
-		'I love java_2e and python programming.'
-	);
-	expect(keywordsFound).toEqual(['java', 'python']);
+	test('replace keywords test', () => {
+		const processor = new KeywordProcessor();
 
-	const newSentence = processor.replaceKeywords(
-		'I prefer python over java programming.'
-	);
-	expect(newSentence).toEqual('I prefer python over java.');
+		processor.addKeyword('Big Apple', 'New York');
+		processor.addKeyword('New Delhi', 'NCR Region');
+
+		const newSentence = processor.replaceKeywords('I love Big Apple and new delhi.');
+		const expected = 'I love New York and NCR Region.';
+
+		expect(newSentence).toEqual(expected);
+	});
+
+	test('case sensitive example test', () => {
+		const processor = new KeywordProcessor(true);
+
+		processor.addKeyword('Big Apple', 'New York');
+		processor.addKeyword('Bay Area');
+
+		const keywordsFound = processor.extractKeywords('I love big Apple and Bay Area.');
+		const expected = ['Bay Area'];
+
+		expect(keywordsFound).toEqual(expected);
+	});
+
+	test('add multiple keywords using Object test', () => {
+		const processor = new KeywordProcessor();
+
+		processor.addKeywordsFromObject({
+			java: ['java_2e', 'java programming'],
+			python: ['python2.7', 'python programming'],
+		});
+
+		const keywordsFound = processor.extractKeywords('I love java_2e and python programming.');
+		expect(keywordsFound).toEqual(['java', 'python']);
+
+		const newSentence = processor.replaceKeywords('I prefer python over java programming.');
+		expect(newSentence).toEqual('I prefer python over java.');
+	});
+
+	test('add multiple keywords using Array test', () => {
+		const processor = new KeywordProcessor();
+
+		processor.addKeywordsFromArray(['java', 'product manager']);
+
+		const keywordsFound = processor.extractKeywords('I am a product manager for a java platform');
+		expect(keywordsFound).toEqual(['product manager', 'java']);
+	});
+
+	test('remove keywords test', () => {
+		const processor = new KeywordProcessor();
+
+		processor.addKeywordsFromArray(['react', 'angular', 'vue', 'javascript']);
+		processor.removeKeywordsFromArray(['angular', 'vue']);
+		processor.removeKeyword('javascript');
+
+		const keywordsFound = processor.extractKeywords('I know React, Angular and Vue. I love JavaScript.');
+
+		expect(keywordsFound).toEqual(['react']);
+	});
+
+	test('add characters as part of word characters', () => {
+		const processor = new KeywordProcessor();
+
+		processor.addKeyword('Big Apple');
+
+		const before = processor.extractKeywords('I love Big Apple/Bay Area.');
+		expect(before).toEqual(['Big Apple']);
+
+		processor.addNonWordBoundaries('/');
+		const after = processor.extractKeywords('I love Big Apple/Bay Area.');
+		expect(after).toEqual([]);
+	});
 });
 
-test('add multiple keywords using Array test', () => {
-	const processor = new KeywordProcessor();
+describe('common use cases', () => {
+	test('shorter search string then keywords', () => {
+		const processor = new KeywordProcessor();
 
-	processor.addKeywordsFromArray(['java', 'product manager']);
+		processor.addKeyword('Google', 'Alphabet');
 
-	const keywordsFound = processor.extractKeywords(
-		'I am a product manager for a java platform'
-	);
-	expect(keywordsFound).toEqual(['product manager', 'java']);
+		const before = 'I work at Google';
+		const after = 'I work at Alphabet';
+		expect(processor.replaceKeywords(before)).toEqual(after);
+	});
 });
 
-test('remove keywords test', () => {
-	const processor = new KeywordProcessor();
+describe('reported edge cases', () => {
+	test('recreating a bug related to sentence ending with a half matching keyword', () => {
+		const processor = new KeywordProcessor();
 
-	processor.addKeywordsFromArray(['react', 'angular', 'vue', 'javascript']);
-	processor.removeKeywordsFromArray(['angular', 'vue']);
-	processor.removeKeyword('javascript');
+		processor.addKeyword('Appl1', 'app_1');
+		processor.addKeyword('Appl2', 'app_2');
+		expect(processor.replaceKeywords('appl1')).toEqual('app_1');
+		expect(processor.replaceKeywords('appl2')).toEqual('app_2');
+		expect(processor.replaceKeywords('app')).toEqual('app');
 
-	const keywordsFound = processor.extractKeywords(
-		'I know React, Angular and Vue. I love JavaScript.'
-	);
+		const sentence = 'I am using an App';
+		const after = processor.replaceKeywords(sentence);
 
-	expect(keywordsFound).toEqual(['react']);
-});
-
-test('add characters as part of word characters', () => {
-	const processor = new KeywordProcessor();
-
-	processor.addKeyword('Big Apple');
-
-	const before = processor.extractKeywords('I love Big Apple/Bay Area.');
-	expect(before).toEqual(['Big Apple']);
-
-	processor.addNonWordBoundaries('/');
-	const after = processor.extractKeywords('I love Big Apple/Bay Area.');
-	expect(after).toEqual([]);
-});
-
-test('recreating a bug related to sentence ending with a half matching keyword', () => {
-	const processor = new KeywordProcessor();
-
-	processor.addKeyword('Appl1', 'app_1');
-	processor.addKeyword('Appl2', 'app_2');
-
-	expect(processor.replaceKeywords('appl1')).toEqual('app_1');
-	expect(processor.replaceKeywords('appl2')).toBe('app_2');
-
-	expect(processor.replaceKeywords('app')).toEqual('app');
-	expect(processor.replaceKeywords('I am using an App')).toEqual(
-		'I am using an App'
-	);
+		expect(after).toEqual(sentence);
+	});
 });
